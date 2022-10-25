932db8c9919091d04590c9eaf19ef07d08a99602
Davi <davi.henrique.silva597@gmail.com>
2022-10-25 01:06:03 -0300

    test: Import Service

---
+++ tests/Chaves.Core.Tests/Services/ImportServiceCoreTest.cs
@@ -0,0 +1,25 @@
+﻿using Chaves.Core.Services;
+using Chaves.Core.ViewModel;
+using Chaves.Core.ViewModel.Enums;
+using FluentAssertions;
+using Xunit;
+
+namespace Chaves.Core.Tests.Services
+{
+    [Trait(nameof(ImportServiceCore), "")]
+    public class ImportServiceCoreTest
+    {
+        [Fact(DisplayName = "Deve Retonar Tela de Falha")]
+        public void DeveRetornarTelaDeFalha()
+        {
+
+            var result = ImportServiceCore.Import("");
+
+            result.Should().NotBeNull();
+
+            result.Should().BeEquivalentTo(new ImportDetailsViewModelCore { Msg = ImporServiceSatus.Fail, Count = 0 });
+
+        }
+
+    }
+}
---
+++ tests/Chaves.Core.Tests/Startup.cs
@@ -0,0 +1,28 @@
+﻿using System.Globalization;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Chaves.Core.Tests
+{
+
+    public class Startup
+    {
+        public IConfiguration Configuracao { get; }
+        public Startup()
+        {
+            Configuracao = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddEnvironmentVariables()
+                .Build();
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSingleton(Configuracao);
+
+            CultureInfo.CurrentCulture = new CultureInfo("pt-BR");
+        }
+    }
+
+}
+
