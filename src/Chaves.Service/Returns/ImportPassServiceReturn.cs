4470f576a13a306e8eaf210d24fac8904fb098d8
Davi <davi.henrique.silva597@gmail.com>
2022-10-25 22:35:28 -0300

    refactor: Ajuste ImportService

---
+++ src/Chaves.Service/Returns/ImportPassServiceReturn.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using Chaves.Data.Models;
+using Chaves.Service.Enums;
+
+namespace Chaves.Service.Returns
+{
+    public class ImportPassServiceReturn
+    {
+        public StatusSource StatusSource { get; set; }
+        public IEnumerable<Password> Passwords { get; set; }
+    }
+}
