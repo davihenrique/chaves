9b5e07f9a3d7bb584d457a03022e12ab52bb6984
Davi <davi.henrique.silva597@gmail.com>
2022-11-05 19:14:46 -0300

    refactor: Default Comparer

---
+++ src/Chaves.Data/Comparer/DefaultPasswordComparer.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using Chaves.Data.Models;
+
+namespace Chaves.Data.Comparer
+{
+    public class DefaultPasswordComparer : IEqualityComparer<Password>
+    {
+        public bool Equals(Password x, Password y)
+        {
+            if (x.Service.Equals(y.Service)
+                && x.User.Equals(y.User)
+                && x.Pass.Equals(y.Pass))
+                return true;
+
+            return false;
+        }
+
+        public int GetHashCode([DisallowNull] Password obj)
+        {
+            var hCode = obj.Service.GetHashCode() ^ obj.User.GetHashCode() ^ obj.Pass.GetHashCode();
+            return hCode.GetHashCode();
+        }
+    }
+}
