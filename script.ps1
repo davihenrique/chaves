2965f670591640f47785d150c967ae592dd8d756
Davi <davi.henrique.silva597@gmail.com>
2022-11-06 19:53:49 -0300

    break

--- script.ps1
+++ script.ps1
@@ -1,17 +1,16 @@
 $scriptDirectory = "./scripts"
 $scripts = Get-ChildItem -Path $scriptDirectory -Recurse | Where-Object {$_.Extension -eq ".ps1"}
-Write-Output $scripts
-
 
 # Empty collection for errors
-$Errors = @()
 
 foreach($script in $scripts)
 {
 
-  [void][System.Management.Automation.Language.Parser]::ParseInput($script,[ref]$null,[ref]$Errors)
-  if($Errors.Count -gt 0){
-    Write-Warning 'Errors found'
+  $Result = Invoke-Pester -Script $script -PassThru
+  if($Result.FailedCount -gt 0)
+  {
+    exit 1
+    break
   }
 
 }
