7b278928e73f4f1e7b500134fd6fe7a2e7fdbdfe
Davi <davi.henrique.silva597@gmail.com>
2022-11-05 21:21:49 -0300

    merge

---
+++ src/Chaves.Service/RecordPasswords/FolderCheck.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using Chaves.Data.Models;
+using CsvHelper;
+using CsvHelper.Configuration;
+
+namespace Chaves.Service.RecordPasswords
+{
+
+    internal static partial class RecordPasswordsService
+    {
+        public static bool CheckFolderChaves()
+        {
+            var diretory = "c:\\chaves";
+            if (!Directory.Exists(diretory))
+                Directory.CreateDirectory("C:\\chaves");
+
+
+            return true;
+        }
+
+        public static bool ExistFile()
+        {
+            var file = "c:\\chaves\\pass.csv";
+
+            if (File.Exists(file))
+                return true;
+
+            var writer = new StreamWriter(file);
+            var csv = new CsvWriter(writer, CultureInfo.InvariantCulture);
+            var pass = new List<Password>();
+
+            csv.WriteRecords(pass);
+            csv.Flush();
+            csv.Dispose();
+
+            return true;
+        }
+
+        public static void RecordCsv(IEnumerable<Password> passwords)
+        {
+            var config = new CsvConfiguration(CultureInfo.InvariantCulture)
+            {
+                HasHeaderRecord = false,
+            };
+
+            var stream = File.Open("c:\\chaves\\pass.csv", FileMode.Append);
+            var writer = new StreamWriter(stream);
+            var csv = new CsvWriter(writer, config);
+
+
+            foreach (var p in passwords)
+                p.Id = new Guid();
+
+            csv.WriteRecords(passwords);
+            csv.Flush();
+            csv.Dispose();
+        }
+
+    }
+}
