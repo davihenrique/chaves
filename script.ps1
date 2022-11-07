0c7f1545984880dbb75d5af9fa1324b44578317d
Davi <davi.henrique.silva597@gmail.com>
2022-11-06 21:42:02 -0300

    DEU RUIM

--- script.ps1
+++ script.ps1
@@ -10,4 +10,10 @@ foreach($script in $scripts)
   $Result = Invoke-Pester -Script $script -PassThru -Quiet
   if($Result.FailedCount -gt 0){ $CODE = 1 }
 }
+
+if($CODE.Equals(1))
+{
+  Write-Error "DEU RUIM" -ErrorAction Stop
+}
+
 exit $CODE
