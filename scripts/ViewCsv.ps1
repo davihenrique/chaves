89dbdcc626e3667684302c8ee6b24b071c62bf98
Davi <davi.henrique.silva597@gmail.com>
2022-11-06 22:38:34 -0300

    Information

--- scripts/ViewCsv.ps1
+++ scripts/ViewCsv.ps1
@@ -4,7 +4,6 @@ if ((Test-Path -Path $file -PathType Leaf)) {
     $PASS = Import-Csv -Path $file
     $PASS | Format-Table
     Write-Output "ROWS:" $PASS.Length
-    
  }
  else {
   Write-Output "NO FILE"
