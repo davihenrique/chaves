b792c76921cc2401fc01aa3812c83f86fbb29cd4
Davi <davi.henrique.silva597@gmail.com>
2022-11-06 21:12:24 -0300

    eeeaafaefasfae fdfd

--- script.ps1
+++ script.ps1
@@ -7,9 +7,15 @@ foreach($script in $scripts)
 {
 
   $Result = Invoke-Pester -Script $script -PassThru
-  if($Result.FailedCount -gt 0)
-  {
-    exit $LASTEXITCODE
-  }
+  if($Result.FailedCount -gt 0){ break }
 
+}
+
+if($Result.FailedCount -gt 0)
+{
+  exit 1
+}
+else
+{
+ exit 0
 }
