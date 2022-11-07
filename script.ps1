72dadeb3133421f0741b10b417fdee17ef118632
Davi <davi.henrique.silva597@gmail.com>
2022-11-06 21:49:14 -0300

    DIFERENTE

--- script.ps1
+++ script.ps1
@@ -1,19 +1,13 @@
 $scriptDirectory = "./scripts"
 $scripts = Get-ChildItem -Path $scriptDirectory -Recurse | Where-Object {$_.Extension -eq ".ps1"}
-
-# Empty collection for errors
-
 $CODE = 0
-
 foreach($script in $scripts)
 {
-  $Result = Invoke-Pester -Script $script -PassThru -Quiet
+  $Result = Invoke-Pester -Script $script -PassThru
   if($Result.FailedCount -gt 0){ $CODE = 1 }
 }
-
 if($CODE.Equals(1))
 {
   Write-Error "DEU RUIM" -ErrorAction Stop
 }
-
 exit $CODE
